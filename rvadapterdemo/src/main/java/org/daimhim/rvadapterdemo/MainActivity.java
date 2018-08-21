5cc28cbbe0b45d1a41c2f5f08ce0d4454239e818
Daimhim <daimhim@163.com>
2018-08-21 10:10:04 +0800

    ipc file demo init

--- rvadapterdemo/src/main/java/org/daimhim/rvadapterdemo/MainActivity.java
+++ rvadapterdemo/src/main/java/org/daimhim/rvadapterdemo/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private ImgAdapter mImgAdapter;
     private StringAdapter mStringAdapter;
     private MixingAdapter mMixingAdapter;
+    private BannerAdapter mBannerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +32,9 @@ public class MainActivity extends AppCompatActivity {
         mImgAdapter = new ImgAdapter(this);
         mStringAdapter = new StringAdapter(this);
         mMixingAdapter = new MixingAdapter(this);
+        mBannerAdapter = new BannerAdapter(this);
 
+        mAdapterManagement.addAdapter(mBannerAdapter);
         mAdapterManagement.addAdapter(mImgAdapter);
         mAdapterManagement.addAdapter(mStringAdapter);
         mAdapterManagement.addAdapter(mMixingAdapter);
@@ -53,6 +56,16 @@ public class MainActivity extends AppCompatActivity {
             pairs.add(new Pair<String, Integer>(String.format("item:%s",i),R.drawable.ic_launcher_background));
         }
         mMixingAdapter.onRefresh(pairs);
+
+        lStrings = new ArrayList<>();
+        lStrings.add("http://i2.17173cdn.com/i7mz64/YWxqaGBf/tu17173com/20150906/PYKLGXbjEqfasyf.jpg");
+        lStrings.add("http://www.k73.com/up/allimg/121005/22-121005113T9404.jpg");
+        lStrings.add("http://pic.962.net/up/2013-5/2013052710010969092.jpg");
+        lStrings.add("http://ossweb-img.qq.com/upload/webplat/info/mh/20150514/1431575482647908.jpg");
+        lStrings.add("https://image.g-cores.com/28e7e731-a7c4-4558-9898-d27df63c0e05.jpg");
+        lStrings.add("http://img.cbigame.com/upload/images/2016/0909/1473388434985279.jpg");
+        lStrings.add("http://pc.xiaopi.com/uploadfile/2017/0324/20170324102538121.jpg");
+        mBannerAdapter.onRefresh(lStrings);
 
         mRvRecycler.setAdapter(mAdapterManagement);
     }
