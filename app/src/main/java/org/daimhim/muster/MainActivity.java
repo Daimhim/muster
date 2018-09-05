941d1b59b8061af323f162ff3c2747c01334d4b7
Daimhim <daimhim@163.com>
2018-09-05 22:51:50 +0800

    添加VirtualAPK插件Demo

--- app/src/main/java/org/daimhim/muster/MainActivity.java
+++ app/src/main/java/org/daimhim/muster/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
     }
 
     private void initView() {
---
+++ hometabdemo/src/main/java/org/daimhim/hometabdemo/FourFragment.java
@@ -0,0 +1,90 @@
+package org.daimhim.hometabdemo;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * 项目名称：org.daimhim.hometabdemo
+ * 项目版本：muster
+ * 创建时间：2018.09.03 15:07  星期一
+ * 创建人：Daimhim
+ * 修改时间：2018.09.03 15:07  星期一
+ * 类描述：Daimhim 太懒了，什么都没有留下
+ * 修改备注：Daimhim 太懒了，什么都没有留下
+ *
+ * @author：Daimhim
+ */
+public class FourFragment extends Fragment {
+    private String TAG = "TAG:"+getClass().getSimpleName();
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG,"onCreateView");
+        return LayoutInflater.from(getContext()).inflate(R.layout.fragment_first,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG,"onViewCreated");
+        TextView tv = view.findViewById(R.id.tv_tag);
+        tv.setText(getArguments().getString("tag"));
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.i(TAG,"onAttach");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG,"onCreate");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG,"onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG,"onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.i(TAG,"onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"onDestroy");
+    }
+
+}
