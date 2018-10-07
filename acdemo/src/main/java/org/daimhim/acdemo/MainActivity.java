3f61cf957c2c2212f7c5a965412d83f8c8057129
daimhim <daimhim@163.com>
2018-10-07 17:51:09 +0800

    init

--- acdemo/src/main/java/org/daimhim/acdemo/MainActivity.java
+++ acdemo/src/main/java/org/daimhim/acdemo/MainActivity.java
@@ -1,9 +1,9 @@
 package org.daimhim.acdemo;
 
-
-import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MainViewModel lMainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        TextView tv = findViewById(R.id.tv_content);
+        tv.setText(TextUtils.concat(
+                getString(R.string.withdrawal_amount), "500.00", "\n",
+                getString(R.string.withdrawal_fee), "500.00", "\n",
+                getString(R.string.total_withdrawal), "500.00", "\n"
+        ).toString());
+//        MainViewModel lMainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
 //        lMainViewModel.
     }
 }
---
+++ lifelonglearning/src/main/java/org/daimhim/lifelonglearning/StartApp.java
@@ -0,0 +1,23 @@
+package org.daimhim.lifelonglearning;
+
+import android.app.Application;
+
+/**
+ * 项目名称：org.daimhim.lifelonglearning
+ * 项目版本：muster
+ * 创建时间：2018/10/7 17:08  星期日
+ * 创建人：Administrator
+ * 修改时间：2018/10/7 17:08  星期日
+ * 类描述：Administrator 太懒了，什么都没有留下
+ * 修改备注：Administrator 太懒了，什么都没有留下
+ *
+ * @author：Administrator
+ */
+public class StartApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
