8c4e19717b919fc295e1e6c3d5a1401292a7f78e
Daimhim <daimhim@163.com>
2018-10-31 22:23:27 +0800

    init

--- pluginmanager/src/main/java/org/daimhim/pluginmanager/ui/plugin/PluginListFragment.java
+++ pluginmanager/src/main/java/org/daimhim/pluginmanager/ui/plugin/PluginListFragment.java
@@ -29,7 +29,8 @@ public class PluginListFragment extends BaseFragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_application, container, false);
         unbinder = ButterKnife.bind(this, view);
         return view;
@@ -38,6 +39,7 @@ public class PluginListFragment extends BaseFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
     }
 
     @Override
